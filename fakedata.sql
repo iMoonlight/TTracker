e52c3a7a1a3db228de44ba77ce10df5392819537
iMoonlight <10297749+iMoonlight@users.noreply.github.com>
2018-11-04 01:32:23 +0200

    added sql file with some fake data

---
+++ fakedata.sql
@@ -0,0 +1,37 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Records of countries
+-- ----------------------------
+INSERT INTO `countries` VALUES (1, 'Австралія', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (2, 'Азербайджан', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (3, 'Алжир', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (4, 'Андорра', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (5, 'Бразилія', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (6, 'Гамбія', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (7, 'Домініка', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (8, 'Еквадор', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (9, 'Екваторіальна Гвінея', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (10, 'Західна Сахара', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (11, 'Ізраїль', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (12, 'Ірак', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (13, 'Канада', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (14, 'Камерун', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+INSERT INTO `countries` VALUES (15, 'Кірибаті', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus arcu id felis auctor, vitae. ');
+
+-- ----------------------------
+-- Records of tourists
+-- ----------------------------
+INSERT INTO `tourists` VALUES (1, 'Peterus Benwell');
+INSERT INTO `tourists` VALUES (2, 'Christabel Bouldstridge');
+INSERT INTO `tourists` VALUES (3, 'Willard Chevers');
+INSERT INTO `tourists` VALUES (4, 'Averill Leuchars');
+INSERT INTO `tourists` VALUES (5, 'Cos Andriveau');
+INSERT INTO `tourists` VALUES (6, 'Adrien Otter');
+INSERT INTO `tourists` VALUES (7, 'Levon Lafferty');
+INSERT INTO `tourists` VALUES (8, 'Bobine Allom');
+INSERT INTO `tourists` VALUES (9, 'Tiffani Ackwood');
+INSERT INTO `tourists` VALUES (10, 'Lori Nazer');
+
+SET FOREIGN_KEY_CHECKS = 1;
